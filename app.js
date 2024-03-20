5c148de1b21022de7572143606bcfa6fd28d6915
David Vaz <davidhvaz@outlook.com>
2024-03-20 17:48:34 -0300

    Primeiro Commit - Correção app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+function criptografar() {
+    let texto = document.querySelector('textarea').value;
+    let criptoE = texto.replace(/e/g, 'enter');
+    console.log(criptoE)
+    let criptoI = criptoE.replace(/i/g, 'imes');
+    console.log(criptoI)
+    let criptoA = criptoI.replace(/a/g, 'ai');
+    console.log(criptoA)
+    let criptoO = criptoA.replace(/o/g, 'ober');
+    console.log(criptoO)
+    let criptoU = criptoO.replace(/u/g, 'ufat');
+    console.log(criptoU)
+    document.getElementById("resultado").value = criptoU;
+    document.getElementById("img").hidden = true;
+    document.getElementById("texto").value = "";
+    document.getElementById("img_tablet").hidden = true;
+
+}
+
+
+function descriptografar() {
+    let texto = document.querySelector('textarea').value;
+    let decriptoE = texto.replace(/enter/g, 'e');
+    let decriptoI = decriptoE.replace(/imes/g, 'i');
+    let decriptoA = decriptoI.replace(/ai/g, 'a');
+    let decriptoO = decriptoA.replace(/ober/g, 'o');
+    let decriptoU = decriptoO.replace(/ufat/g, 'u');
+    document.getElementById("resultado").value = decriptoU;
+    document.getElementById("img").hidden = true;
+    document.getElementById("texto").value = "";
+    document.getElementById("img_tablet").hidden = true;
+
+}
+
+function copiar() {
+    let texto = document.getElementById("resultado");
+    texto.select();
+    texto.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(texto.value);
+    document.getElementById("resultado").value = "";
+
+
+}
